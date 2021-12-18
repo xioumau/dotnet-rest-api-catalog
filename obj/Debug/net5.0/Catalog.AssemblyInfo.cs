ad4ac7a9e4e2c071c49d7a62a3ecbfe0f8af5714
mauricio <mauriciocon@gmail.com>
2021-12-18 15:44:18 -0300

    enable authentication in MongoDB | use .NET Secret Manager

--- obj/Debug/net5.0/Catalog.AssemblyInfo.cs
+++ obj/Debug/net5.0/Catalog.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("a034949a-5062-4f49-8059-c2e26d0e243e")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
